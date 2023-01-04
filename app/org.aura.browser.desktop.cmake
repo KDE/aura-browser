929d8bd8428ebc727b7e7cbc80d312ff2120f42f
l10n daemon script <scripty@kde.org>
2023-01-04 04:27:45 +0000

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- app/org.aura.browser.desktop.cmake
+++ app/org.aura.browser.desktop.cmake
@@ -3,6 +3,7 @@ Name=Aura Browser
 Name[ar]=متصفح أورا
 Name[ca]=Navegador Aura
 Name[ca@valencia]=Navegador Aura
+Name[cs]=Prohlížeč Aura
 Name[de]=Aura Browser
 Name[en_GB]=Aura Browser
 Name[es]=Navegador Aura
